cfa26ba9ffbdf01789c6c71fc330547bcc952cd9
gertrudeahoulou <151026898+gertrudeahoulou@users.noreply.github.com>
2023-11-17 11:50:12 +0100

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,8 @@
+using Systeme;
+ static void Main(string[] args)
+ {
+    int number = 70;
+     if (number > 0)
+ }     if (number > 10)
+           if (number == 70)
+    console.writeline( "super");
